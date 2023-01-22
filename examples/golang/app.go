8848b75a3ba8da01588d83a65194ca7ce6c4a58a
manuel_teves <manuel.tomas.teves@gmail.com>
2023-01-21 22:42:35 -0300

    Update

---
+++ examples/golang/app.go
@@ -0,0 +1,7 @@
+package main
+
+import TPress "github.com/TevesManuel/TPress/golang"
+
+func main() {
+	TPress.TypeText("Hello world")
+}
---
+++ examples/golang/go.mod
@@ -0,0 +1,5 @@
+module TPress
+
+go 1.19
+
+require github.com/TevesManuel/TPress/golang v0.0.0-20230121182426-8f6b416e4515 // indirect
